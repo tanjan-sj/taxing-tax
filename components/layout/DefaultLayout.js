5bed3a46e6c88bf55b5bb586fb23df6e240ec9a8
Death-Gear <backsideburner@gmail.com>
2021-10-27 14:07:41 +0600

    Add theme to change primary color of application

--- components/layout/DefaultLayout.js
+++ components/layout/DefaultLayout.js
@@ -1,9 +1,10 @@
-import {Toolbar} from "@mui/material";
+import {ThemeProvider, Toolbar} from "@mui/material";
 import * as React from "react";
 import {useState} from "react";
 import NavBar from "./NavBar";
 import TemporaryDrawer from "./TemporaryDrawer";
 import PermanentDrawer from "./PermanentDrawer";
+import CustomTheme from "../theme/CustomTheme";
 
 export default function DefaultLayout({children}) {
   const [sidebar, setSidebar] = useState(false);
@@ -19,18 +20,20 @@ export default function DefaultLayout({children}) {
   };
 
   return (
-    <div style={{display: 'flex'}}>
-      <NavBar toggleDrawer={toggleDrawer} sidebar={sidebar} />
-      <PermanentDrawer />
-      <TemporaryDrawer toggleDrawer={toggleDrawer} sidebar={sidebar} />
+    <ThemeProvider theme={CustomTheme}>
+      <div style={{display: 'flex'}}>
+        <NavBar toggleDrawer={toggleDrawer} sidebar={sidebar} />
+        <PermanentDrawer />
+        <TemporaryDrawer toggleDrawer={toggleDrawer} sidebar={sidebar} />
 
-      <main style={{
-        flexGrow: 1,
-        padding: '24px'
-      }}>
-        <Toolbar/>
-        {children}
-      </main>
-    </div>
+        <main style={{
+          flexGrow: 1,
+          padding: '24px'
+        }}>
+          <Toolbar/>
+          {children}
+        </main>
+      </div>
+    </ThemeProvider>
   )
 }
